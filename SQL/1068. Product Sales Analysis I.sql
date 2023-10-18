996264f2f2ab9dc614bf7620df834883ae601232
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-10-18 17:07:48 +0530

    Create 1068. Product Sales Analysis I.sql

---
+++ SQL/1068. Product Sales Analysis I.sql
@@ -0,0 +1,41 @@
+-- https://leetcode.com/problems/product-sales-analysis-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+Return the resulting table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT product_name, year, price FROM Sales
+LEFT JOIN Product ON Sales.product_id = Product.product_id;
