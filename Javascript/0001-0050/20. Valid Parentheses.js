884eb44b14289ecb392caa48f8bbaa99f5d14ced
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-15 16:20:51 +0530

    Update 20. Valid Parentheses.js

--- Javascript/0001-0050/20. Valid Parentheses.js
+++ Javascript/0001-0050/20. Valid Parentheses.js
@@ -1,44 +1,42 @@
 // https://leetcode.com/problems/valid-parentheses/description/
 
-class Solution {
-    public boolean isValid(String s) {
-        //store the parantheses in the stack
-        Stack<Character> storage = new Stack<>();
+var isValid = function(s) {
+    //store the parantheses in the stack
+    storage = [];
 
-        //lets traverse throught the string
-        for(int index=0; index<s.length(); index++){
-            //store the characters
-            char bracket = s.charAt(index);
+    //lets traverse throught the string
+    for(let index=0; index<s.length; index++){
+        //store the characters
+        bracket = s[index];
 
-            //check for opening paranthesis
-            if(bracket == '{' || bracket == '(' || bracket == '['){
-                storage.push(bracket);
-            }
-            //check for valid paranthesis in stack
-            else if(bracket == '}'){
-                if(storage.isEmpty() || storage.peek() != '{'){
-                    return false;
-                }
-                storage.pop(); //remove from stack as its valid
-            }
-            else if(bracket == ']'){
-                if(storage.isEmpty() || storage.peek() != '['){
-                    return false;
-                }
-                storage.pop(); //remove from stack as its valid
-            }
-            else if(bracket == ')'){
-                if(storage.isEmpty() || storage.peek() != '('){
-                    return false;
-                }
-                storage.pop(); //remove from stack as its valid
-            }
+        //check for opening paranthesis
+        if(bracket == '{' || bracket == '(' || bracket == '['){
+            storage.push(bracket);
         }
-        //final check is there any un-closed paranthesis in the stack
-        if(!storage.isEmpty()){
-            return false; //unclosed paranthesis is there in stack
+        //check for valid paranthesis in stack
+        else if(bracket == '}'){
+            if(storage.length == 0 || storage[storage.length-1] != '{'){
+                return false;
+            }
+            storage.pop(); //remove from stack as its valid
         }
-
-        return true;
+        else if(bracket == ']'){
+            if(storage.length == 0 || storage[storage.length-1] != '['){
+                return false;
+            }
+            storage.pop(); //remove from stack as its valid
+        }
+        else if(bracket == ')'){
+            if(storage.length == 0 || storage[storage.length-1] != '('){
+                return false;
+            }
+            storage.pop(); //remove from stack as its valid
+        }
     }
-}
+    //final check is there any un-closed paranthesis in the stack
+    if(storage.length != 0){
+        return false; //unclosed paranthesis is there in stack
+    }
+
+    return true;
+};
