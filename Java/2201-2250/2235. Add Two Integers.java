4b35de1245e3e1d774f69b02cfa18e6899ba8981
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-26 09:49:03 +0530

    Update 2235. Add Two Integers.java

--- Java/2201-2250/2235. Add Two Integers.java
+++ Java/2201-2250/2235. Add Two Integers.java
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/add-two-integers/
 
-class Solution:
-    def sum(self, num1: int, num2: int) -> int:
-        return num1 + num2
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
