d8dac62107f168921c3906706f4fd4b2d7794fe8
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-12-13 21:30:57 +0530

    Create 374. Guess Number Higher or Lower.java

---
+++ Java/0351-0400/374. Guess Number Higher or Lower.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        while(start<=end) {
+            int mid = start + (end-start) /2;
+            if(guess(mid) == 0)
+                return mid;
+            else if(guess(mid) == 1)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+}
