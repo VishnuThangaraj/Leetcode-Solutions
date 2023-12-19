ac1cd96f44602fe8704cb94fcc2346f7126b6845
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-12-19 15:06:37 +0530

    Create 182. Duplicate Emails.sql

---
+++ SQL/182. Duplicate Emails.sql
@@ -0,0 +1,21 @@
+-- https://leetcode.com/problems/duplicate-emails/
+
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+*/
+
+select `Email` from `Person` group by `Email` having count(*) > 1;
