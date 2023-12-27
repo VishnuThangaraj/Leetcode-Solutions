40a496be13e65bc280fcbd70fc99c8c071977e7b
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-12-27 20:43:34 +0530

    Create 175. Combine Two Tables.sql

---
+++ SQL/175. Combine Two Tables.sql
@@ -0,0 +1,36 @@
+-- https://leetcode.com/problems/combine-two-tables/
+
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+ 
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+Return the result table in any order.
+*/
+
+SELECT firstName, lastName, city, state FROM Person LEFT JOIN Address USING(PersonId);
