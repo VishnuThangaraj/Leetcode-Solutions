3006935cc823e8cc9fc80cc46722fe82e2d82b43
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-12-28 09:02:03 +0530

    Create 183. Customers Who Never Order.sql

---
+++ SQL/183. Customers Who Never Order.sql
@@ -0,0 +1,34 @@
+-- https://leetcode.com/problems/customers-who-never-order/
+
+/*
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+*/
+
+SELECT name AS Customers FROM Customers WHERE id NOT IN (SELECT DISTINCT customerId FROM Orders);
