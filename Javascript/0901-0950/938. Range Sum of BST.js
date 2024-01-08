e62350e810ab3203f748f93260d73104fec1843d
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-01-08 09:21:45 +0530

    Create 938. Range Sum of BST.js

---
+++ Javascript/0901-0950/938. Range Sum of BST.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/range-sum-of-bst/
+
+// https://leetcode.com/problems/range-sum-of-bst/solutions/4526122/o-n-java-python-javascript-typescript-c-c-binary-search-tree/
+
+var rangeSumBST = function(root, low, high) {
+    if (root == null)
+        return 0;
+
+    if(root.val < low)
+        return rangeSumBST(root.right, low, high);
+    else if(root.val > high)
+        return rangeSumBST(root.left, low, high);
+
+    return root.val + rangeSumBST(root.right, low, high)+rangeSumBST(root.left, low, high);
+};
