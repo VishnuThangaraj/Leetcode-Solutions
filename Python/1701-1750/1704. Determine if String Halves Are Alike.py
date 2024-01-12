dfcec741eb9a2dfe5ff51e1435ed6674766c9cdb
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-01-12 07:45:14 +0530

    Create 1704. Determine if String Halves Are Alike.py

---
+++ Python/1701-1750/1704. Determine if String Halves Are Alike.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/
+
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/solutions/4549382/o-n-java-python-javascript-typescript-c-c-string-matching/
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        vowels = set('aeiouAEIOU')
+        size = len(s)
+        count = 0
+        
+        for index in range(0, size//2):
+            if s[index] in vowels:
+                count += 1
+
+            if s[index + size//2] in vowels:
+                count -= 1
+        
+        return count == 0
