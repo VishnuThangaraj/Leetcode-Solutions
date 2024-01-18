3ee5cfcf79f046003ed6a1606582c53637542757
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-01-18 08:04:17 +0530

    Update 70. Climbing Stairs.js

--- Javascript/0051-0100/70. Climbing Stairs.js
+++ Javascript/0051-0100/70. Climbing Stairs.js
@@ -2,23 +2,21 @@
 
 // https://leetcode.com/problems/climbing-stairs/solutions/4584499/o-n-o-1-java-python-javascript-typescript-c-c-fibonacci-method/
 
-class Solution {
-    public int climbStairs(int n) {
-        // If the N is less than 2 | No.of ways to reach will be N.
-        if(n<=2) return n;
+var climbStairs = function(n) {
+    // If the N is less than 2 | No.of ways to reach will be N.
+    if(n<=2) return n;
 
-        // Initialize variables to keep track of previous two steps.
-        int prev = 1, B_prev = 2;
-        // Current Step Count based on previous Two steps.
-        int current = 0;
+    // Initialize variables to keep track of previous two steps.
+    let prev = 1, B_prev = 2;
+    // Current Step Count based on previous Two steps.
+    let current = 0;
 
-        for(int loop=1; loop<n-1; loop++){
-            current = prev + B_prev; // Calculate current steps
-            // Update previous steps for next iteration
-            prev = B_prev;
-            B_prev = current;
-        }
-
-        return current;
+    for(let loop=1; loop<n-1; loop++){
+        current = prev + B_prev; // Calculate current steps
+        // Update previous steps for next iteration
+        prev = B_prev;
+        B_prev = current;
     }
-}
+
+    return current;
+};
